e3eef95835150d7cc3cdf61e4f1f0888beb61faf
laszlocsonka <43496943+laszlocsonka@users.noreply.github.com>
2018-09-26 18:06:22 +0200

    Add files via upload

---
+++ Elsohazi/src/Pont.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LacGep
+ */
+public class Pont {
+     private double x;
+    private double y;
+
+    public Pont(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return this.x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+    
+}
